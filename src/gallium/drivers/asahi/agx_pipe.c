ffaa38ff4c348a1e26ed95ab9ff10426fb652c15
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2023-06-05 22:23:35 -0400

    XXX: asahi: Disable Z/S compression with STK
    Hot fix for https://github.com/supertuxkart/stk-code/issues/4863
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>

--- src/gallium/drivers/asahi/agx_pipe.c
+++ src/gallium/drivers/asahi/agx_pipe.c
@@ -35,6 +35,7 @@
 #include "util/u_gen_mipmap.h"
 #include "util/u_inlines.h"
 #include "util/u_memory.h"
+#include "util/u_process.h"
 #include "util/u_screen.h"
 #include "util/u_upload_mgr.h"
 #include "util/xmlconfig.h"
@@ -436,6 +437,15 @@ agx_compression_allowed(const struct agx_resource *pres)
    if (agx_device(pres->base.screen)->debug & AGX_DBG_NOCOMPRESS) {
       rsrc_debug(pres, "No compression: disabled\n");
       return false;
+   }
+
+   /* Workaround for https://github.com/supertuxkart/stk-code/issues/4863
+    *
+    * XXX: Fix upstream or at least driconf, this is terrible.
+    */
+   if (strcmp(util_get_process_name(), "supertuxkart") == 0) {
+      if (pres->base.bind & PIPE_BIND_DEPTH_STENCIL)
+         return false;
    }
 
    /* Limited to renderable */
